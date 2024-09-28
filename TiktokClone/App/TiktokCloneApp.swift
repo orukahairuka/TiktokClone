8270e35f890030495392f78d1ce5901615d9d0af
irukahaoruka <erika03301109@gmail.com>
2024-09-28 22:41:52 +0900

    1

---
+++ TiktokClone/App/TiktokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokCloneApp.swift
+//  TiktokClone
+//
+//  Created by 櫻井絵理香 on 2024/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TiktokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

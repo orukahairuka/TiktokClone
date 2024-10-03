b6cc2af891a9e92ea9c704b72ba30acbcfd94a15
irukahaoruka <erika03301109@gmail.com>
2024-10-03 15:59:44 +0900

    a

---
+++ TiktokClone/Core/Feeds/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  TiktokClone
+//
+//  Created by 櫻井絵理香 on 2024/10/03.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}

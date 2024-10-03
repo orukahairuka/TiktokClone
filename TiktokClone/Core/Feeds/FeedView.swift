8a5c5ea269b2b6b55793c040fa9b0347fa06b389
irukahaoruka <erika03301109@gmail.com>
2024-10-03 15:52:37 +0900

    a

---
+++ TiktokClone/Core/Feeds/FeedView.swift
@@ -0,0 +1,32 @@
+//
+//  FeedView.swift
+//  TiktokClone
+//
+//  Created by 櫻井絵理香 on 2024/10/03.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack (spacing: 0){
+                ForEach(0 ..< 10) { post in
+                    Rectangle()
+                        .fill(.pink)
+                        .containerRelativeFrame([.horizontal, .vertical])
+                        .overlay {
+                            Text("Post \(post)")
+                                .foregroundStyle(.white)
+                        }
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.paging)
+    }
+}
+
+#Preview {
+    FeedView()
+}
